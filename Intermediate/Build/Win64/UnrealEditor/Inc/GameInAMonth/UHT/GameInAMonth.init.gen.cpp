1a535700d63c79699069afd91d3c14d82c62cd39
ConnorMPage <94052838+ConnorMPage@users.noreply.github.com>
2024-05-04 20:57:05 +0100

    Add project files.

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/GameInAMonth/UHT/GameInAMonth.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeGameInAMonth_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_GameInAMonth;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_GameInAMonth()
+	{
+		if (!Z_Registration_Info_UPackage__Script_GameInAMonth.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/GameInAMonth",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x20B154EA,
+				0x956EC92E,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_GameInAMonth.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_GameInAMonth.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_GameInAMonth(Z_Construct_UPackage__Script_GameInAMonth, TEXT("/Script/GameInAMonth"), Z_Registration_Info_UPackage__Script_GameInAMonth, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x20B154EA, 0x956EC92E));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
